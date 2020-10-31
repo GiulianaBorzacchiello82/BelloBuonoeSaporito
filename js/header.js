b1e5869b43a834af8954eede511d855e56a93724
GiulianaBorzacchiello82 <34265970+GiulianaBorzacchiello82@users.noreply.github.com>
2020-10-31 23:15:24 +0100

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,24 @@
+var evt = isMobile() ? 'touchend' : 'click';
+
+$(document).ready(function() {
+	
+    $(".menu .nav #ricette").on("mouseenter", function(){
+    	$("#subHeader span.desc").addClass("hide");
+    	$("#subHeader span.desc").removeClass("hide");
+    })
+    .on("mouseleave", function(e) {
+    	var mouseleavetimeout = $(this).data("mouseleavetimeout");
+    	if (mouseleavetimeout) {
+    		clearTimeout(mouseleavetimeout);
+    	}
+    	mouseleavetimeout = setTimeout(function() {
+    		if ( !$(e.relateTarget).hasClass("userMenuList") ) {
+          $("#subHeader span.desc").removeClass("hide");
+      	  $("#subHeader span.desc").addClass("hide");
+        	}
+    	}, 0);
+    	$(this).data("mouseleavetimeout", mouseleavetimeout);
+    });
+    
+
+}
